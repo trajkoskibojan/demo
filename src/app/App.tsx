4810a8792b01a5488ec7dcd0c7bdf10a963e748a
DESKTOP-27MRO0B\bojan <bojan.trajkoski.mk@gmail.com>
2021-08-22 13:34:06 +0200

    browser router setting

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import Repo from './views/Repo';
+import * as React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Repo from './views/Repo/Repo';
 
-const App: React.FC = () => <Repo />;
+const App: React.FC = () => (
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Repo} />
+        </Switch>
+    </BrowserRouter>
+);
 
 export default App;
